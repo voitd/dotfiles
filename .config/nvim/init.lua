da5692e957196d873e9bd5f129deb764f770bdc6
voitd <dimytr.voitenko@gmail.com>
2021-01-11 17:06:42 +0200

    feat: added some aliases

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,37 +1,16 @@
-local U = require('utils')
--- local D = os.getenv('DOTFILES') or os.getenv('HOME')..'/.dotfiles'
-local D = os.getenv('HOME')..'/.config'
+local D = os.getenv("HOME") .. "/.config"
 local o = vim.o
 
-U.disable_distribution_plugins()
 -- This is a magic line that will take your pain away.
-o.rtp = string.format('%s/nvim,%s', D, o.rtp)
+o.rtp = string.format("%s/nvim,%s", D, o.rtp)
+
+-- LSP
+-- require "lsp"
 
 -- Vanilla Config
-require 'plugins'
-require 'settings'
-require 'autocmd'
-require 'keybinds'
-require 'opts'
+require "settings"
 
 -- Plugins Config
-require 'plugins/_colorizer'
-require "plugins/_devicons"
-require 'plugins/_floaterm'
-require "plugins/_formatter"
-require 'plugins/_fzf'
-require "plugins/_sign"
-require "plugins/_startify"
-require "plugins/_st"
-require "plugins/_coc"
-require "plugins/_rooter"
-require 'plugins/_nord'
-require 'plugins/_sneak'
--- require "plugins/_rigel"
-require "plugins/_lsp"
--- require 'plugins/_tree'
--- require "plugins/_completions"
-require "plugins/_diagnostics"
--- require 'plugins/_tsitter'
--- require "plugins/_"
+require "plugins"
+
 
--- .config/nvim/lua/plugins/_rooter.lua
+++
@@ -1,8 +0,0 @@
-require'rooter'.setup {
-    echo = false,
-    patterns = { -- the patterns to find
-      '.git',    -- same as patterns passed to nvim_lsp.util.root_pattern(patterns...)
-      'package.json',
-    },
-    non_project_files = "current"
-  }
